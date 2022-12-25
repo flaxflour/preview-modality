5bcbc194e9c8f2a0fd314e365737a631a53ce17f
Adva Alkalay <adva.a@dataloop.ai>
2022-12-25 22:36:25 +0200

    v1.0.12

--- patch.py
+++ patch.py
@@ -3,12 +3,13 @@ import subprocess
 
 with open('./dataloop.json') as json_file:
     data = json.load(json_file)
-    prefix = data['version'][:-1]
-    last_number = int(data['version'][-1])
-    data['version'] = f'{prefix}{last_number + 1}'
+    current_version = data['version'].split('.')
+    current_version[-1] = str(int(current_version[-1]) + 1)
+    data['version'] = '.'.join(current_version)
     data['source']['tag'] = f'v{data["version"]}'
     current_version = data['source']['tag']
     print(current_version)
+    
 with open("./dataloop.json", "w") as jsonFile:
     json.dump(data, jsonFile, indent=2)
 
