db1ec49a1045063459e76696232370af3a723c8b
Adva Alkalay <adva.a@dataloop.ai>
2022-12-25 22:31:12 +0200

    build script update - edit index.html assets

--- build.py
+++ build.py
@@ -12,3 +12,13 @@ src_dir = 'dist'
 dest_dir = f'panels/{panel_name}'
 files = os.listdir(src_dir)
 shutil.copytree(src_dir, dest_dir)
+
+lines = []
+with open(f'./panels/{panel_name}/index.html', 'r') as html_file:
+    for line in html_file.readlines():
+        if "/assets/" in line:
+            line = line.replace('/assets/', f'../{panel_name}/assets/')
+        lines.append(line)
+
+with open(f'./panels/{panel_name}/index.html', 'w') as updated_html_file:
+    updated_html_file.writelines(lines)
